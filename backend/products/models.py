e3d887bed351e5f9bf79c8c05a9f4fd3bf67bc1e
Jacobo Mateo Bedoya Oquendo <jacobomateobedoya@icloud.com>
2024-03-28 11:02:56 -0500

    FEAT: giving form to the shift and the production order

--- backend/products/models.py
+++ backend/products/models.py
@@ -15,4 +15,27 @@ from base.models import BaseModel
 
 
 class Product(BaseModel):
-    ...
+    """
+    """
+    name = models.CharField(
+        blank=False,
+        null=False,
+        max_length=255,
+    )
+    materials = models.JSONField(
+        blank=False,
+        null=True,
+    )
+    production_time = models.TimeField(
+        blank=False,
+        null=True,
+    )
+
+
+class ProductionOrder(BaseModel):
+    """
+    """
+    client_id = ...
+    product = ...
+    product_ammount = ...
+    delivery_date = ...
