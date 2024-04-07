b1953e17f5dc310683c427be000d79f9230c57de
Jacobo Mateo Bedoya Oquendo <jacobomateobedoya@icloud.com>
2024-04-07 13:30:15 -0500

    FEAT: Added some more things for the "clients" module.

---
+++ backend/clients/urls.py
@@ -0,0 +1,13 @@
+"""Machines module endpoints.
+"""
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+    # path(
+    #     "roles/",
+    #     views.employee_roles_view,
+    #     name="roles"
+    # ),
+]
