3623d0077a85b0f4a4490c13111d7f318865dbd4
Jacobo Mateo Bedoya Oquendo <jacobomateobedoya@icloud.com>
2024-05-27 18:53:14 -0500

    MEH: just generic stuff

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,25 +1,4 @@
-from typing import (
-    Union,
-    Iterable,
-    Optional,
-    List,
-    Dict,
-)
-from django.http import (
-    HttpRequest,
-    JsonResponse,
-)
 from django.utils.translation import gettext as _
-from django.views.decorators.http import require_http_methods
-from django.views.decorators.http import (
-    require_GET,
-    require_POST,
-)
-from django.db.models import Q
-from django.http import JsonResponse
-
-from base.http_status_codes import HTTP_STATUS as status
-from base.logger import base_logger
 from base.generic_views import (
     BaseListView,
     BaseCreateView,
@@ -27,8 +6,6 @@ from base.generic_views import (
     BaseUpdateView,
     BaseDeleteView,
 )
-from jwt_authentication.decorators import authenticated_user
-from employees.decorators import role_validation
 
 from employees.models import RoleChoices
 from employees.mixins import (
@@ -47,7 +24,7 @@ from .forms import (
 
 class ProductListView(AuthenticatedUserMixin, BaseListView):
     """
-    Class based view to handle the filtering and listing the objects.
+    View Class based view to handle the filtering and listing the objects.
     """
     model = Product
     form = ProductForm
@@ -56,7 +33,7 @@ class ProductListView(AuthenticatedUserMixin, BaseListView):
 
 class ProductCreationView(RoleValidatorMixin, BaseCreateView):
     """
-    Class to chandle the creation of the Product model objects.
+    View Class to chandle the creation of the Product model objects.
     """
     allowed_roles = [
         RoleChoices.PRODUCTION_MANAGER,
@@ -66,10 +43,15 @@ class ProductCreationView(RoleValidatorMixin, BaseCreateView):
     form = ProductCreationForm
     serializer_depth = 0
 
+
 class ProductDetailView(AuthenticatedUserMixin, BaseDetailView):
+    """
+    View Class to handle the Detailed Product View.
+    """
     model = Product
     serializer_depth = 0
     url_kwarg: str = "id"
+
 
 class ProductUpdateDeleteView(
     RoleValidatorMixin,
@@ -77,8 +59,8 @@ class ProductUpdateDeleteView(
     BaseDeleteView,
 ):
     """
-    Class to handle the deatiled view, the update and the delete of the Product
-    model object.
+    View Class to handle the deatiled view, the update and the delete of the
+    Product model object.
     """
     allowed_roles = [
         RoleChoices.PRODUCTION_MANAGER,
