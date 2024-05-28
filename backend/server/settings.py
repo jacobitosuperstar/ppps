70e42a96213cda5672cb636a4e366da71a0b17ec
Jacobo Mateo Bedoya Oquendo <jacobomateobedoya@icloud.com>
2024-05-27 22:10:26 -0500

    FEAT: Fixing the docker-compose once again

--- backend/server/settings.py
+++ backend/server/settings.py
@@ -96,6 +96,14 @@ WSGI_APPLICATION = 'server.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -106,7 +114,6 @@ DATABASES = {
         'PORT': '5432',  # Post specified in the docker-compose
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
